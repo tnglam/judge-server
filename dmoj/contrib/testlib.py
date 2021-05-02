c0a4adc11d0be0846ab18cc3803f76a98e2ef2c2
Le Duy Thuc <leduythuc.cs@gmail.com>
2021-05-02 13:08:56 +0700

    More feedback on testlib bridged

--- dmoj/contrib/testlib.py
+++ dmoj/contrib/testlib.py
@@ -31,13 +31,13 @@ class ContribModule(DefaultContribModule):
             points = float(match.group(1))
             print(points)
             if not 0 <= points <= point_value:
-                raise InternalError('Invalid partial points: %d' % points)
+                raise InternalError('Invalid partial points: %f, must be between [%f; %f]' % (points, 0, point_value))
             return CheckerResult(True, points, feedback=feedback, extended_feedback=extended_feedback)
         elif proc.returncode == cls.WA:
             return CheckerResult(False, 0, feedback=feedback, extended_feedback=extended_feedback)
         elif proc.returncode == cls.PE:
             return CheckerResult(False, 0, feedback=feedback or 'Presentation Error', extended_feedback=extended_feedback)
         elif proc.returncode == cls.IE:
-            raise InternalError('%s failed assertion with message %s' % (name, feedback))
+            raise InternalError('%s failed assertion with message %s %s' % (name, feedback, extended_feedback))
         else:
             parse_helper_file_error(proc, executor, name, stderr, time_limit, memory_limit)
