41b4062301a7612110646c3e5927b9426b51ef1b
Le Duy Thuc <leduythuc.cs@gmail.com>
2022-02-23 15:41:01 +0000

    themis: catch casting error

--- dmoj/contrib/themis.py
+++ dmoj/contrib/themis.py
@@ -1,4 +1,5 @@
 from dmoj.contrib.default import ContribModule as DefaultContribModule
+from dmoj.error import InternalError
 from dmoj.result import CheckerResult
 from dmoj.utils.helper_files import parse_helper_file_error
 
@@ -16,8 +17,13 @@ class ContribModule(DefaultContribModule):
         if proc.returncode != cls.AC:
             parse_helper_file_error(proc, executor, name, stderr, time_limit, memory_limit)
         else:
-            # Don't need to strip() here because extended_feedback has already stripped.
-            points = float(extended_feedback.split('\n')[-1]) * point_value
+            try:
+                # Don't need to strip() here because extended_feedback has already stripped.
+                points = float(extended_feedback.split('\n')[-1]) * point_value
+            except ValueError as e:
+                # In case the last line is not a float number, raise Internal Error
+                # so that the wrapper catch_internal_error will catch the exception
+                raise InternalError(e)
             # TODO (thuc): We should check 0 <= points <= point_value, but I don't want to raise an internal error
             # So I skip the check.
 
